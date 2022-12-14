a5aed306c441daa52ba38f4a34473221b25c6a95
OnyiOrji <onyiorji1234@gmail.com>
2022-12-14 10:10:31 -0600

    moved index to react folder

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App">Lets get started
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
