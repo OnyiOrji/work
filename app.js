d02d04fca770c269596cca04d80953a2aba70272
OnyiOrji <onyiorji1234@gmail.com>
2022-12-13 20:16:00 -0600

    java testing

--- app.js
+++ app.js
@@ -5,3 +5,11 @@ const printMyName = name => {
     console.log(name)
 }
 printMyName('Max');
+const numbers = [1,2,3];
+
+const doubleNumArray = numbers.map((num) => {
+return num * 2
+});
+
+console.log(numbers)
+console.log(doubleNumArray)
