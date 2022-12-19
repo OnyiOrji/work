6c46f991b83d6956dd963a2a95b0ce5488cb526c
OnyiOrji <onyiorji1234@gmail.com>
2022-12-19 13:58:26 -0600

    added react imports on all react components

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -34,10 +34,10 @@ function App() {
 
   // return (
   // <div>
-  {/* <h2>Let's get started!</h2> */ }
-  {/* <Expenses items={expenses} /> */ }
-  {/* </div> */ }
-  // );
+  //<h2>Let's get started!</h2> 
+  //{<Expenses items={expenses} /> }
+  //{ </div>  }
+  // ); */
 }
 
 export default App;
