7facd2ff73c9ea86d7697b873b63e4dd70fd457b
OnyiOrji <onyiorji1234@gmail.com>
2023-01-01 12:05:52 -0600

    intergrated data storage for user input

--- 01-starting-setup/src/components/NewExpense/ExpenseForm.js
+++ 01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -35,8 +35,18 @@ const ExpenseForm = () => {
         //     enteredDate: event.target.value
         // })
     };
+    const submitHandler = (event) => {
+        event.preventDefault();
 
-    return <form>
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        };
+        console.log(expenseData)
+    };
+
+    return <form onSubmit={submitHandler}>
         <div className="new-expense__controls">
             <div className="new-expense__control">
                 <label>Title</label>
