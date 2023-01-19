295908498a432a96d29dc2f409d200b95db35a73
OnyiOrji <onyiorji1234@gmail.com>
2023-01-19 09:41:23 -0600

    added new c++ demo file

---
+++ OO_helloworld.cpp
@@ -0,0 +1,13 @@
+//>>Onyinyechukwu Orji<<
+ 
+//>>01/18/2023<<
+ 
+//Assignment 1: Hello World!
+ 
+#include <iostream>
+using namespace std;
+ 
+int main() {
+	cout << "Hello world!\nMy name is Onyinyechukwu Orji \n";
+	return 0;
+}
