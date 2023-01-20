8d981849d5b62634f01efbda0da94f1772286f7a
OnyiOrji <onyiorji1234@gmail.com>
2023-01-20 09:52:53 -0600

    initialized a method to get previous block

--- python/Blockchain.py
+++ python/Blockchain.py
@@ -26,5 +26,7 @@ class Blockchain:
                  'previous_hash': previous_hash}
         self.chain.append(block)
         return block
-        
+    
+    def get_previous_block(self):
+        return self.chain[-1]
 # Part 2 - Mining our Blockchain
