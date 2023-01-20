2a54b7ca7ab82cf3d5a941b3c52de2380a8bd095
OnyiOrji <onyiorji1234@gmail.com>
2023-01-20 10:48:25 -0600

    Integrated proof of work cryptographical puzzle

--- python/Blockchain.py
+++ python/Blockchain.py
@@ -14,11 +14,11 @@ from flask import Flask, jsonify
 
 
 class Blockchain:
-
+# initialze self method
     def __init__(self):
         self.chain = []
         self.create_block(proof=1, previous_hash='0')
-
+# initialize create a block function
     def create_block(self, proof, previous_hash):
         block = {'index': len(self.chain) + 1,
                  'timestamp': str(datetime.datetime.now()),
@@ -26,7 +26,18 @@ class Blockchain:
                  'previous_hash': previous_hash}
         self.chain.append(block)
         return block
-    
+# initialize get previous block function    
     def get_previous_block(self):
         return self.chain[-1]
+# initialize proof of work crytpographical function   
+    def proof_of_work(self, previous_proof):
+        new_proof = 1
+        check_proof = False
+        while check_proof is False:
+            hash_operation = hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexdigest()
+            if hash_operation[:4] == '0000':
+                check_proof = True
+            else:
+                new_proof += 1           
+        return new_proof
 # Part 2 - Mining our Blockchain
