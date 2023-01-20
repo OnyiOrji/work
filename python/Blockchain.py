9897a5623cb587ccb7b32d526afa884c1fe673e9
OnyiOrji <onyiorji1234@gmail.com>
2023-01-20 09:24:23 -0600

    Building Blockchain

---
+++ python/Blockchain.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+Author: Onyinyechukwu Orji
+Create a Blockchain
+"""
+import datetime
+import hashlib
+import json
+from flask import Flask, jsonify
+
+# Part 1 - Building a Blockchain
+class Blockchain:
+    
+    def __init__(self):
+        self.chain = []
+        self.create_block(proof=1, previous_hash='0')
+
+# Part 2 - Mining our Blockchain
