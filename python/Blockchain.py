5ea2b011f9699a370314e9daa4f5a8bd2f2829df
OnyiOrji <onyiorji1234@gmail.com>
2023-02-13 11:02:50 -0600

    import request & uuid libraries for transaction validation

--- python/Blockchain.py
+++ python/Blockchain.py
@@ -8,76 +8,125 @@ Create a Blockchain
 import datetime
 import hashlib
 import json
-from flask import Flask, jsonify
-#from flask import Flask, waitress
+from flask import Flask, jsonify, request
+import requests
+from uuid import uuid4
+from urllib.parse import urlparse
+# from flask import Flask, waitress
 
 # Part 1 - Building a Blockchain
 
 
 class Blockchain:
-# initialze self method
+    # initialze self method
     def __init__(self):
         self.chain = []
+        self.transactions = []
         self.create_block(proof=1, previous_hash='0')
+        self.nodes = set()
 # initialize create a block function
+
     def create_block(self, proof, previous_hash):
         block = {'index': len(self.chain) + 1,
                  'timestamp': str(datetime.datetime.now()),
                  'proof': proof,
-                 'previous_hash': previous_hash}
+                 'previous_hash': previous_hash,
+                 'transactions': self.transactions}
         self.chain.append(block)
         return block
-# initialize get previous block function    
+# initialize get previous block function
+
     def get_previous_block(self):
         return self.chain[-1]
-# initialize proof of work crytpographical function   
+# initialize proof of work crytpographical function
+
     def proof_of_work(self, previous_proof):
         new_proof = 1
         check_proof = False
         while check_proof is False:
-            hash_operation = hashlib.sha256(str(new_proof**2 - previous_proof**2).encode()).hexdigest()
+            hash_operation = hashlib.sha256(
+                str(new_proof**2 - previous_proof**2).encode()).hexdigest()
             if hash_operation[:4] == '0000':
                 check_proof = True
             else:
-                new_proof += 1           
+                new_proof += 1
         return new_proof
 # validate hash chain
+
     def hash(self, block):
-        encoded_block = json.dumps(block, sort_keys = True).encode()
+        encoded_block = json.dumps(block, sort_keys=True).encode()
         return hashlib.sha256(encoded_block).hexdigest()
 # validate new block on blockchain
+
     def is_chain_valid(self, chain):
         previous_block = chain[0]
         block_index = 1
-        while block_index < len(chain): 
+        while block_index < len(chain):
             block = chain[block_index]
             if block['previous_hash'] != self.hash(previous_block):
                 return False
             previous_proof = previous_block['proof']
             proof = block['proof']
-            hash_operation = hashlib.sha256(str(proof**2 - previous_proof**2).encode()).hexdigest()
+            hash_operation = hashlib.sha256(
+                str(proof**2 - previous_proof**2).encode()).hexdigest()
             if hash_operation[:4] != '0000':
                 return False
             previous_block = block
             block_index += 1
         return True
-            
+# validate new block transactions
+
+    def add_transaction(self, sender, receiver, amount):
+        self.transactions.append({'sender': sender,
+                                  'receiver': receiver,
+                                  'amount': amount})
+        previous_block = self.get_previous_block()
+        return previous_block['index'] + 1
+
+    def add_node(self, address):
+        parsed_url = urlparse(address)
+        self.nodes.add(parsed_url.netloc)
+
+    def replace_chain(self):
+        network = self.nodes
+        longest_chain = None
+        max_length = len(self.chain)
+        for node in network:
+            response = requests.get(f'http://{node}/get_chain')
+            if response.status_code == 200:
+                length = response.json()['length']
+                chain = response.json()['chain']
+                if length > max_length and self.is_chain_valid(chain):
+                    max_length = length
+                    longest_chain = chain
+        if longest_chain:
+            self.chain = longest_chain
+            return True
+        return False
+
 # Part 2 - Mining our Blockchain
 
-#Creating a Web App
+
+# Creating a Web App
 app = Flask(__name__)
 
+# Creating an address for the node on Port 8080
+node_address = str(uuid4()).replace('-', '')
 
-#Creating a Blockchain
+# Creating a Blockchain
 blockchain = Blockchain()
 
-#Mining a new Block
-@app.route('/mine_block', methods = ['GET'])
+# Mining a new Block
+
+
+@app.route('/mine_block', methods=['GET'])
 def mine_block():
     previous_block = blockchain.get_previous_block()
     previous_proof = previous_block['proof']
     proof = blockchain.proof_of_work(previous_proof)
     previous_hash = blockchain.hash(previous_block)
+    blockchain.add_transaction(
+        sender=node_address, receiver='Hadelin', amount=100)
     block = blockchain.create_block(proof, previous_hash)
     response = {'message': 'Congratulations, You Mined A Block!',
                 'index': block['index'],
@@ -88,14 +137,18 @@ def mine_block():
     return jsonify(response), 200
 
 # Getting the full Blockchain
-@app.route('/get_chain', methods = ['GET'])
+
+
+@app.route('/get_chain', methods=['GET'])
 def get_chain():
     response = {'chain': blockchain.chain,
                 'length': len(blockchain.chain)}
     return jsonify(response), 200
 
 # Check if Blockchain is valid
-@app.route('/is_valid', methods = ['GET'])
+
+
+@app.route('/is_valid', methods=['GET'])
 def is_valid():
     is_valid = blockchain.is_chain_valid(blockchain.chain)
     if is_valid:
@@ -103,9 +156,10 @@ def is_valid():
     else:
         response = {'message': 'Error! Chain NOT valid'}
     return jsonify(response)
-    
+
+
 # Running the app
-#if __name__ == "__main__":
-    #from waitress import serve
-    #serve(app, host="0.0.0.0", port=8080)
-app.run(host= '0.0.0.0', port=(8080))
+# if __name__ == "__main__":
+    # from waitress import serve
+    # serve(app, host="0.0.0.0", port=8080)
+app.run(host='0.0.0.0', port=(8080))
