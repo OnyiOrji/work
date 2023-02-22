8fc4fa7912bfefc5ae93ada9a580b926c69d0803
OnyiOrji <onyiorji1234@gmail.com>
2023-02-22 09:06:59 -0600

    connecting new node class method

--- python/Blockchain_2.py
+++ python/Blockchain_2.py
@@ -170,7 +170,14 @@ def add_transaction():
 @app.route('/connect_node', methods=['POST'])
 def connect_node():
     json = request.get_json()
-
+    nodes = json.get('nodes')
+    if nodes is None:
+        return "No node", 400
+    for node in nodes:
+        blockchain.add_node(node)
+    response = {'message': 'All the nodes are now connected. The WCI Coin Blockchain now contains the following nodes. ',
+                'total_nodes': list(blockchain.nodes)}
+    return jsonify(response)
 
 # Running the app
 # if __name__ == "__main__":
